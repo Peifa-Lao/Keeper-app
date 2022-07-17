d82f4e74f3f985a6b685ae7065546d801febcfa3
Nick(Peifa) Lao <nick-laopeifa@outlook.com>
2022-07-17 15:09:27 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-//create a react devolopment environment
+//create a react devolopment environment, "App /" is component. Using component is easy to manage.
 ReactDOM.render(<App />, document.getElementById("root"));
