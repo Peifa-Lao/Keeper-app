73a8929cb7f3888691838c28432d52d8007683cb
Nick(Peifa) Lao <nick-laopeifa@outlook.com>
2022-07-17 14:32:19 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
+//create a react devolopment environment
 ReactDOM.render(<App />, document.getElementById("root"));
