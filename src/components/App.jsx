5aab90e620f1b8c941be3d9327b9c6e366658f00
Nick(Peifa) Lao <nick-laopeifa@outlook.com>
2022-08-15 18:13:30 -0700

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,9 @@ import CreateArea from "./CreateArea";
 function App() {
   const [notes, setNotes] = useState([]);
   
-  //receive the object from createArea.jsx
- 
+  //receive the newNote object and add into the notes
   function addNote(newNote) {
+    //add newNote to notes
     setNotes(prevNotes => {
       return [...prevNotes, newNote];
     });
@@ -17,6 +17,7 @@ function App() {
 
   function deleteNote(id) {
     setNotes(prevNotes => {
+      //first return is return to notes above for update
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
@@ -30,14 +31,19 @@ function App() {
     //create area component and {} is javascript expression
     //receiving data from CreateArea and calling addNote function in app.jsx
       <CreateArea onAdd={addNote} />
+
      //{} indicates javascript 
      //use arrow function to pass Note to map throught all array of notes  
      //map function is going to loop throught the notes array
-     //noteItem has index, or can use UUID
+     //map function can provide index for id or using uuid
+     //noteItem represent each of items in notes
+   
+
       {notes.map((noteItem, index) => {
         return (
           //render note component and create a new note using the properties from that note
-          //pass properties to Node component 
+          //pass properties to Node component
+          //pass props(key,id, title,content,ondelete) to Node component
           <Note
             key={index}
             id={index}
